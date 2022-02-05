69b323bebbcce5cf700a120aae5771f6f8bf5963
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2022-02-05 22:26:53 +0330

    update-database-5

---
+++ src/TwitchNightFall.Core/Infra.Data/Migrations/20220205185340_Update-Databaee-5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TwitchNightFall.Core.Migrations
+{
+    public partial class UpdateDatabase5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiredAt",
+                schema: "ray",
+                table: "Subscription",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                schema: "ray",
+                table: "Administrator",
+                keyColumn: "Id",
+                keyValue: new Guid("c0915809-b937-4e84-b7ba-97efcf9af77c"),
+                columns: new[] { "CreatedAt", "ModifiedAt" },
+                values: new object[] { new DateTime(2022, 2, 5, 18, 53, 40, 59, DateTimeKind.Utc).AddTicks(4935), new DateTime(2022, 2, 5, 18, 53, 40, 59, DateTimeKind.Utc).AddTicks(4936) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiredAt",
+                schema: "ray",
+                table: "Subscription");
+
+            migrationBuilder.UpdateData(
+                schema: "ray",
+                table: "Administrator",
+                keyColumn: "Id",
+                keyValue: new Guid("c0915809-b937-4e84-b7ba-97efcf9af77c"),
+                columns: new[] { "CreatedAt", "ModifiedAt" },
+                values: new object[] { new DateTime(2022, 2, 5, 18, 10, 1, 751, DateTimeKind.Utc).AddTicks(6824), new DateTime(2022, 2, 5, 18, 10, 1, 751, DateTimeKind.Utc).AddTicks(6826) });
+        }
+    }
+}
