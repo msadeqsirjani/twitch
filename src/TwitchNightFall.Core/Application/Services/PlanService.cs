23d26ffd39dc43863042f2aaab03575ccde896ca
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2022-02-05 21:37:56 +0330

    rename plan service

---
+++ src/TwitchNightFall.Core/Application/Services/PlanService.cs
@@ -0,0 +1,43 @@
+﻿using Gridify;
+using TwitchNightFall.Core.Application.Common;
+using TwitchNightFall.Core.Application.Services.Plan;
+
+namespace TwitchNightFall.Core.Application.Services;
+
+public interface IPlanService
+{
+    Result ShowPlans(GridifyQuery request);
+}
+
+public class PlanService : IPlanService
+{
+    private readonly List<IPlan> _subscriptions;
+
+    public PlanService()
+    {
+        _subscriptions = FillSubscriptions();
+    }
+
+    public Result ShowPlans(GridifyQuery request)
+    {
+        return Result.WithSuccess(_subscriptions.AsQueryable().Gridify(request));
+    }
+
+    private static List<IPlan> FillSubscriptions()
+    {
+        return new List<IPlan>
+        {
+            new Plan750Monthly(),
+            new Plan600Monthly(),
+            new Plan450Monthly(),
+            new Plan300Monthly(),
+            new Plan150Monthly(),
+            new Plan175Weekly(),
+            new Plan140Weekly(),
+            new Plan70Weekly(),
+            new Plan10Round(),
+            new Plan20Round(),
+            new Plan50Round()
+        };
+    }
+}
